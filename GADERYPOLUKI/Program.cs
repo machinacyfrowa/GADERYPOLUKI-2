ec79d30a28454c0c8288bb4cb93003c3b3fe0550
pzawadzki <admin@machinacyfrowa.pl>
2023-03-27 12:31:37 +0200

    move logic to method

--- GADERYPOLUKI/Program.cs
+++ GADERYPOLUKI/Program.cs
@@ -1,33 +1,41 @@
-﻿string text = "ALA MA KOTA";
+﻿string gaderypoluki(string text)
+{
+    //para G->A, A->G
+    text = text.Replace('G', '@');
+    text = text.Replace('A', 'G');
+    text = text.Replace('@', 'A');
 
-//para G->A, A->G
-text = text.Replace('G', '@');
-text = text.Replace('A', 'G');
-text = text.Replace('@', 'A');
+    //para D->E, E->D
+    text = text.Replace('D', '@');
+    text = text.Replace('E', 'D');
+    text = text.Replace('@', 'E');
 
-//para D->E, E->D
-text = text.Replace('D', '@');
-text = text.Replace('E', 'D');
-text = text.Replace('@', 'E');
+    //para R->Y, Y->R
+    text = text.Replace('R', '@');
+    text = text.Replace('Y', 'R');
+    text = text.Replace('@', 'Y');
 
-//para R->Y, Y->R
-text = text.Replace('R', '@');
-text = text.Replace('Y', 'R');
-text = text.Replace('@', 'Y');
+    //para P->O, O->P
+    text = text.Replace('P', '@');
+    text = text.Replace('O', 'P');
+    text = text.Replace('@', 'O');
 
-//para P->O, O->P
-text = text.Replace('P', '@');
-text = text.Replace('O', 'P');
-text = text.Replace('@', 'O');
+    //para L->U, U->L
+    text = text.Replace('L', '@');
+    text = text.Replace('U', 'L');
+    text = text.Replace('@', 'U');
 
-//para L->U, U->L
-text = text.Replace('L', '@');
-text = text.Replace('U', 'L');
-text = text.Replace('@', 'U');
+    //para K->I, I->K
+    text = text.Replace('L', '@');
+    text = text.Replace('U', 'L');
+    text = text.Replace('@', 'U');
 
-//para K->I, I->K
-text = text.Replace('L', '@');
-text = text.Replace('U', 'L');
-text = text.Replace('@', 'U');
+    return text;
+}
+
+
+string text = "ALA MA KOTA";
+
+text = gaderypoluki(text);
 
 Console.WriteLine(text);
