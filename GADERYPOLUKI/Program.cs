8afe7b98b1c8254d9351d31427b567ce67358633
pzawadzki <admin@machinacyfrowa.pl>
2023-03-27 12:27:08 +0200

    Dodaj pliki projektów.

---
+++ GADERYPOLUKI/Program.cs
@@ -0,0 +1,33 @@
+﻿string text = "ALA MA KOTA";
+
+//para G->A, A->G
+text = text.Replace('G', '@');
+text = text.Replace('A', 'G');
+text = text.Replace('@', 'A');
+
+//para D->E, E->D
+text = text.Replace('D', '@');
+text = text.Replace('E', 'D');
+text = text.Replace('@', 'E');
+
+//para R->Y, Y->R
+text = text.Replace('R', '@');
+text = text.Replace('Y', 'R');
+text = text.Replace('@', 'Y');
+
+//para P->O, O->P
+text = text.Replace('P', '@');
+text = text.Replace('O', 'P');
+text = text.Replace('@', 'O');
+
+//para L->U, U->L
+text = text.Replace('L', '@');
+text = text.Replace('U', 'L');
+text = text.Replace('@', 'U');
+
+//para K->I, I->K
+text = text.Replace('L', '@');
+text = text.Replace('U', 'L');
+text = text.Replace('@', 'U');
+
+Console.WriteLine(text);
